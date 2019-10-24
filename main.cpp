b2e51b5f606ae1f7b6f5b2d95c5c0c278ec2994c
GabrielOlem <gme@cin.ufpe.br>
2019-10-23 23:37:55 -0300

    multiple object tracking with kalman

--- main.cpp
+++ main.cpp
@@ -4,28 +4,32 @@
 #include <opencv2/core.hpp>
 #include <eigen3/Eigen/Eigen>
 #include <iostream>
+#include <pthread.h>
+#include <bits/stdc++.h>
+
+#define NUM_OBJECTS 2
 
 using namespace cv;
 using namespace Eigen;
 using namespace std;
+struct object{
+  Matrix<double, 4, 1> teste;
+  MatrixXd X, A, B, C, Ez, Ex, K, lido, P;
+  int H_MIN, H_MAX, S_MIN, S_MAX, V_MIN, V_MAX;
+  double aream, areama;
+};
+Mat yuvimage;
 
-MatrixXd X(4, 1);
-MatrixXd A(4, 4);
-MatrixXd B(4, 1);
-MatrixXd C(2, 4);
-MatrixXd Ez(2, 2);
-MatrixXd Ex(4, 4);
-MatrixXd K(4, 4);
+object objetos[NUM_OBJECTS];
 
-int H_MIN = 110; //5 52
-int H_MAX = 169;//19 75 
-int S_MIN = 31;//138 84
-int S_MAX = 87;//204 120
-int V_MIN = 0;//251 188
-int V_MAX = 255;//256 210
+
 //bola laranja 110 to 169  31 to 87 and 0 to 255
 //bola amarela 208 to 236
 //bola vermelha 52 to 75 84 to 120 and 188 to 210
+
+Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3,3));
+Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8,8));
+
 void on_trackbar( int, void* )
 {//This function gets called whenever a
 	// trackbar position is changed
@@ -35,124 +39,176 @@ void on_trackbar( int, void* )
 
 
 }
-void createTrackbars(){
+void createTrackbars(int i){
 	//create window for trackbars
 
 
     namedWindow("trackbarWindowName",0);
 	//create memory to store trackbar name on window
 	char TrackbarName[50];
-	sprintf( TrackbarName, "H_MIN", H_MIN);
-	sprintf( TrackbarName, "H_MAX", H_MAX);
-	sprintf( TrackbarName, "S_MIN", S_MIN);
-	sprintf( TrackbarName, "S_MAX", S_MAX);
-	sprintf( TrackbarName, "V_MIN", V_MIN);
-	sprintf( TrackbarName, "V_MAX", V_MAX);
+	sprintf( TrackbarName, "H_MIN", objetos[i].H_MIN);
+	sprintf( TrackbarName, "H_MAX", objetos[i].H_MAX);
+	sprintf( TrackbarName, "S_MIN", objetos[i].S_MIN);
+	sprintf( TrackbarName, "S_MAX", objetos[i].S_MAX);
+	sprintf( TrackbarName, "V_MIN", objetos[i].V_MIN);
+	sprintf( TrackbarName, "V_MAX", objetos[i].V_MAX);
 	//create trackbars and insert them into window
 	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
 	//the max value the trackbar can move (eg. H_HIGH), 
 	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
 	//                                  ---->    ---->     ---->      
-    createTrackbar( "H_MIN", "trackbarWindowName", &H_MIN, H_MAX, on_trackbar );
-    createTrackbar( "H_MAX", "trackbarWindowName", &H_MAX, H_MAX, on_trackbar );
-    createTrackbar( "S_MIN", "trackbarWindowName", &S_MIN, S_MAX, on_trackbar );
-    createTrackbar( "S_MAX", "trackbarWindowName", &S_MAX, S_MAX, on_trackbar );
-    createTrackbar( "V_MIN", "trackbarWindowName", &V_MIN, V_MAX, on_trackbar );
-    createTrackbar( "V_MAX", "trackbarWindowName", &V_MAX, V_MAX, on_trackbar );
+    createTrackbar( "H_MIN", "trackbarWindowName", &objetos[i].H_MIN, objetos[i].H_MAX, on_trackbar );
+    createTrackbar( "H_MAX", "trackbarWindowName", &objetos[i].H_MAX, objetos[i].H_MAX, on_trackbar );
+    createTrackbar( "S_MIN", "trackbarWindowName", &objetos[i].S_MIN, objetos[i].S_MAX, on_trackbar );
+    createTrackbar( "S_MAX", "trackbarWindowName", &objetos[i].S_MAX, objetos[i].S_MAX, on_trackbar );
+    createTrackbar( "V_MIN", "trackbarWindowName", &objetos[i].V_MIN, objetos[i].V_MAX, on_trackbar );
+    createTrackbar( "V_MAX", "trackbarWindowName", &objetos[i].V_MAX, objetos[i].V_MAX, on_trackbar );
 
 
 }
-int main( int argc, char** argv ) {
-  A << 1,0,1,0,
+void init(int i){
+  objetos[i].X.resize(4, 1);
+  objetos[i].A.resize(4, 4);
+  objetos[i].B.resize(4, 1);
+  objetos[i].C.resize(2, 4);
+  objetos[i].Ez.resize(2, 2);
+  objetos[i].Ex.resize(4, 4);
+  objetos[i].K.resize(4, 4);
+  objetos[i].lido.resize(4, 1);
+  objetos[i].P.resize(4, 4);
+
+  objetos[i].A << 1,0,1,0,
        0,1,0,1,
        0,0,1,0,
        0,0,0,1;
-  B << 0.5,0.5,1,1;
-  C << 1,0,0,0,
+  objetos[i].B << 0.5,0.5,1,1;
+  objetos[i].C << 1,0,0,0,
        0,1,0,0;
-  Ez << 0.0025, 0,
+  objetos[i].Ez << 0.0025, 0,
         0, 0.0025;
-  Ex << 1/4,0,1/2,0,
+  objetos[i].Ex << 1/4,0,1/2,0,
         0,1/4,0,1/2,
         1/2,0,1,0,
         0,1/2,0,1;
-  X << 0,0,0,0;
-  MatrixXd lido(4,1);
-  MatrixXd P(4,4);
-  lido << 0,0,0,0;
-  Mat image, yuvimage, cinzado, frame;
+  objetos[i].X << 0,0,0,0;
+  objetos[i].lido << 0,0,0,0;
+  if(i == 0){
+    objetos[i].H_MIN = 109; //5 52
+    objetos[i].H_MAX = 138;//19 75
+    objetos[i].S_MIN = 86;//138 84
+    objetos[i].S_MAX = 101;//204 120
+    objetos[i].V_MIN = 0;//251 188
+    objetos[i].V_MAX = 255;//256 210
+    objetos[i].aream = 900;
+    objetos[i].areama = 1150;
+  }
+  if(i == 1){
+    objetos[i].H_MIN = 110; //5 52
+    objetos[i].H_MAX = 169;//19 75
+    objetos[i].S_MIN = 31;//138 84
+    objetos[i].S_MAX = 87;//204 120
+    objetos[i].V_MIN = 0;//251 188
+    objetos[i].V_MAX = 255;//256 210
+    objetos[i].aream = 2500;
+    objetos[i].areama = 4500;
+  }
+}
+void *op(void *n){
+  int numero = *((int*) n);
+  Mat cinzado;
+  inRange(yuvimage, Scalar(objetos[numero].H_MIN, objetos[numero].S_MIN, objetos[numero].V_MIN), Scalar(objetos[numero].H_MAX, objetos[numero].S_MAX, objetos[numero].V_MAX), cinzado);
+  
+  erode(cinzado, cinzado, erodeElement);
+  erode(cinzado, cinzado, erodeElement);
+    
+  dilate(cinzado, cinzado, dilateElement);
+  dilate(cinzado, cinzado, dilateElement);
+  Mat temp;
+  cinzado.copyTo(temp);
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+  findContours(temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+  bool objectFound = false;
+  int numObjects = 0;
+  objetos[numero].X = objetos[numero].A*objetos[numero].X;
+  objetos[numero].P = objetos[numero].A*objetos[numero].P*objetos[numero].A.transpose() + objetos[numero].Ex;
+  objetos[numero].K = objetos[numero].P*objetos[numero].C.transpose()*(objetos[numero].C*objetos[numero].P*objetos[numero].C.transpose() + objetos[numero].Ez).inverse();
+  if(hierarchy.size() > 0){
+    numObjects = hierarchy.size();
+  }
+  printf("Numero de objetos %i\n", numObjects);
+  if(numObjects < 5 && numObjects > 0){
+    for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+      Moments moment = moments((cv::Mat)contours[index]);
+      double area = moment.m00;
+
+      printf("Area %lf\n", area);
+      if(area>objetos[numero].aream && area<objetos[numero].areama){
+        double a, b;
+        a = objetos[numero].lido(0);
+        b = objetos[numero].lido(1);
+        objetos[numero].lido(0) = moment.m10/area;
+        objetos[numero].lido(1) = moment.m01/area;
+        objetos[numero].lido(2) = objetos[numero].lido(0) - a;
+        objetos[numero].lido(3) = objetos[numero].lido(1) - b;
+        objectFound = true;
+      }
+      else objectFound = false;
+
+
+    }
+    //let user know you found an object
+    if(objectFound == true){
+      MatrixXd teste(2,1);
+      teste(0) = objetos[numero].lido(0);
+      teste(1) = objetos[numero].lido(1);
+      objetos[numero].X = objetos[numero].X + objetos[numero].K*(teste - objetos[numero].C*objetos[numero].X);
+    }
+    else{
+      objetos[numero].lido(0) = -1;
+    }
+    
+  }
+  else{
+    cout << "TOO MUCH NOISE! ADJUST FILTER" << endl;
+  }
+  MatrixXd I(4,4);
+  I << 1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1;
+  objetos[numero].P = (I - objetos[numero].K*objetos[numero].C)*objetos[numero].P;
+  pthread_exit(NULL);
+}
+int main( int argc, char** argv ) {
+  for(int i=0; i<NUM_OBJECTS; i++){
+    init(i);
+  }
+  Mat image, frame;
   VideoCapture print("robocin.webm");
   //createTrackbars();
   print.read(image);
   imshow("a", image);
-  //createTrackbars();
-  Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3,3));
-  Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8,8));
   while(1){
     cvtColor(image, yuvimage, COLOR_BGR2YUV);
-    inRange(yuvimage, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), cinzado);
-    erode(cinzado, cinzado, erodeElement);
-    erode(cinzado, cinzado, erodeElement);
+    pthread_t threads[NUM_OBJECTS];
     
-    dilate(cinzado, cinzado, dilateElement);
-    dilate(cinzado, cinzado, dilateElement);
-    
-    imshow("cinz", cinzado);
-    Mat temp;
-    cinzado.copyTo(temp);
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours(temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
-    bool objectFound = false;
-    int numObjects = 0;
-    X = A*X;
-    P = A*P*A.transpose() + Ex;
-    K = P*C.transpose()*(C*P*C.transpose() + Ez).inverse();
-    if(hierarchy.size() > 0){
-      numObjects = hierarchy.size();
+    for(int i=0; i<NUM_OBJECTS; i++){
+      int *n = new int;
+      *n = i;
+      pthread_create(&threads[i], NULL, op, (void*)n);
     }
-    printf("Numero de objetos %i\n", numObjects);
-    if(numObjects < 5 && numObjects > 0){
-      for (int index = 0; index >= 0; index = hierarchy[index][0]) {
-
-				Moments moment = moments((cv::Mat)contours[index]);
-				double area = moment.m00;
-
-        printf("Area %lf\n", area);
-        if(area>2500 && area<4500){
-					double a, b;
-          a = lido(0);
-          b = lido(1);
-          lido(0) = moment.m10/area;
-					lido(1) = moment.m01/area;
-          lido(2) = lido(0) - a;
-          lido(3) = lido(1) - b;
-					objectFound = true;
-				}
-        else objectFound = false;
-
-
-			}
-			//let user know you found an object
-			if(objectFound == true){
-        MatrixXd teste(2,1);
-        teste(0) = lido(0);
-        teste(1) = lido(1);
-        X = X + K*(teste - C*X);
-				circle(image,Point(lido(0),lido(1)),30,Scalar(0,255,0),2);
+    for(int i=0; i<NUM_OBJECTS; i++){
+      pthread_join(threads[i], NULL);
+    }
+    for(int i=0; i<NUM_OBJECTS; i++){
+      if(objetos[i].lido(0) != -1){
+        circle(image,Point(objetos[i].lido(0),objetos[i].lido(1)),30,Scalar(0,255,0),2);
       }
-      
-		}
-    else{
-      cout << "TOO MUCH NOISE! ADJUST FILTER" << endl;
+      circle(image, Point(objetos[i].X(0), objetos[i].X(1)), 20, Scalar(0, 0, 255), 5);
     }
-    circle(image, Point(X(0), X(1)), 20, Scalar(0, 0, 255), 5);
-    MatrixXd I(4,4);
-    I << 1,0,0,0,
-         0,1,0,0,
-         0,0,1,0,
-         0,0,0,1;
-    P = (I - K*C)*P;
+    
     imshow("a", image);
     if(waitKey(0) == 27){
       print.read(image);
